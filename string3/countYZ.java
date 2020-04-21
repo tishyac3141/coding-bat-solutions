4366923e134ce7f39a0af33cb5ec176f29946f33
Tishya Chhabra <tatooine888@gmail.com>
2020-04-21 14:03:46 -0700

    added countYZ

---
+++ string3/countYZ.java
@@ -0,0 +1,19 @@
+package string3;
+
+public class countYZ {
+
+    public int countYZ(String str){
+        int count = 0;
+        str = str.toLowerCase();
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) ==  'y' || str.charAt(i) == 'z'){
+
+                if( (i + 1) >= str.length() ) count++;
+                else if(!(Character.isLetter(str.charAt(i)))) count++;
+            }
+        }
+
+        return count;
+    }
+}
