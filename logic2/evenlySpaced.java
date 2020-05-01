62ae1f86ad165dae82b9c7c131e961585b3717fa
Tishya Chhabra <tatooine888@gmail.com>
2020-04-30 21:44:41 -0700

    added some classes

---
+++ logic2/evenlySpaced.java
@@ -0,0 +1,44 @@
+package logic2;
+
+public class evenlySpaced {
+ 
+    public boolean evenlySpaced(int a, int b, int c) {
+        int large;
+        int small;
+        int medium;
+        if(a > b && a > c){
+         large = a;
+         if(b < c){
+           small = b;
+           medium = c;
+         } else {
+           small = c;
+           medium = b;
+         }
+        } 
+        else if(b > a && b > c){
+          large = b;
+          if(a < c){
+            small = a;
+            medium = c;
+          } else {
+            small = c;
+            medium = a;
+          }
+        }
+        else{
+          large = c;
+          if(a < b){
+            small = a;
+            medium = b;
+          } else{
+            small = b;
+            medium = a;
+          }
+        }
+        
+        return Math.abs(small - medium) == large - medium;
+        
+     }
+
+}
