48642550335d86496a11dc7d6db76c607b126b3d
Tishya Chhabra <tatooine888@gmail.com>
2020-06-01 22:28:48 -0700

    created + finished scoresIncreasing

---
+++ ap1/scoresIncreasing.java
@@ -0,0 +1,17 @@
+package ap1;
+
+//codingbat link: https://codingbat.com/prob/p146974
+
+public class scoresIncreasing {
+
+    public boolean scoresIncreasing(int[] scores) {
+        boolean isIncreasing = true;
+  
+        for(int i = 1; i < scores.length; i++){
+            if(scores[i - 1] > scores[i]) isIncreasing = false;
+        }
+        
+        return isIncreasing;
+    }
+    
+}
