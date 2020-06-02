311974780ea9b76155fb2f91eae3c8cbd1bfbdda
Tishya Chhabra <tatooine888@gmail.com>
2020-06-01 22:24:27 -0700

    created string1 + helloName

---
+++ string1/helloName.java
@@ -0,0 +1,11 @@
+package string1;
+
+//codingbat link: https://codingbat.com/prob/p171896
+
+public class helloName {
+
+    public String helloName(String name){
+        return "Hello " + name + "!";
+    }
+    
+}
