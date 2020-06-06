af570398843bcd7edd5da4163149f3b0f082b53e
Tishya Chhabra <tatooine888@gmail.com>
2020-06-05 21:33:45 -0700

    created and finished sleepIn

---
+++ warmup1/sleepIn.java
@@ -0,0 +1,11 @@
+package warmup1;
+
+//codingbat link: https://codingbat.com/prob/p187868
+
+public class sleepIn {
+    
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+    
+}
