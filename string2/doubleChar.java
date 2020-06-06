1aefeb1ba49742235883140a7a6318a9ff6565e9
Tishya Chhabra <tatooine888@gmail.com>
2020-06-05 21:31:52 -0700

    created and finished doubleChar

---
+++ string2/doubleChar.java
@@ -0,0 +1,18 @@
+package string2;
+
+//codingbat link: https://codingbat.com/prob/p165312
+
+public class doubleChar {
+
+    public String doubleChar(String str){
+        String doubleString = "";
+  
+        for(int i = 0; i < str.length(); i++){
+            doubleString += str.charAt(i);
+            doubleString += str.charAt(i);
+        }
+        
+        return doubleString; 
+    }
+    
+}
