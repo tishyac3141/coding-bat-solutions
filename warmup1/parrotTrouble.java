7012d9809d7048beee65fb1f52b5304304397609
Tishya Chhabra <tatooine888@gmail.com>
2020-06-07 18:28:55 -0700

    created and finished parrotTrouble

---
+++ warmup1/parrotTrouble.java
@@ -0,0 +1,11 @@
+package warmup1;
+
+//codingbat link: https://codingbat.com/prob/p140449
+
+public class parrotTrouble {
+
+    public boolean parrotTrouble(boolean talking, int hour){
+        return talking && (hour < 7 || hour > 20);
+    }
+    
+}
