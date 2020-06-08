600c6226d3fb61a4ccdfc966f94eab835f908448
Tishya Chhabra <tatooine888@gmail.com>
2020-06-07 18:25:28 -0700

    created and finished diff21

---
+++ warmup1/diff21.java
@@ -0,0 +1,12 @@
+package warmup1;
+
+//codingbat link: https://codingbat.com/prob/p116624
+
+public class diff21 {
+
+    public int diff21(int n){
+        if(n > 21) return Math.abs(2*(n-21));
+        else return Math.abs(21-n);
+    }
+    
+}
