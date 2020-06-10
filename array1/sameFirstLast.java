33aa7fc906cb97f385a31d217ed901e64e50dd54
Tishya Chhabra <tatooine888@gmail.com>
2020-06-10 08:38:56 -0700

    created and finished sameFirstLast

---
+++ array1/sameFirstLast.java
@@ -0,0 +1,12 @@
+package array1;
+
+//codingbat link: https://codingbat.com/prob/p118976
+
+public class sameFirstLast {
+
+    public boolean sameFirstLast(int[] nums) {
+        return nums.length >= 1 && nums[0] == nums[nums.length-1];
+    }
+    
+    
+}
