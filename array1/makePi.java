d91f89cac3e12caa2c60f43f4e3e850482574e8f
Tishya Chhabra <tatooine888@gmail.com>
2020-06-10 08:41:38 -0700

    created and finished makePi

---
+++ array1/makePi.java
@@ -0,0 +1,12 @@
+package array1;
+
+//codingbat link: https://codingbat.com/prob/p167011
+
+public class makePi {
+    
+    public int[] makePi() {
+        return new int[] {3, 1, 4};
+    }
+    
+    
+}
