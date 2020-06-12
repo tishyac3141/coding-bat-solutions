b2bafb79e2a4a746e83eebca21cbf58321b5a9f2
Tishya Chhabra <tatooine888@gmail.com>
2020-06-12 14:05:12 -0700

    created and finished squirrelPlay

---
+++ logic1/squirrelPlay.java
@@ -0,0 +1,10 @@
+package logic1;
+
+public class squirrelPlay {
+
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        if(isSummer) return temp >= 60 && temp <= 100;
+        else return temp >= 60 && temp <= 90;
+      }
+    
+}
