73fa6b97cd6f3c4aa31d8947c74a56031f192520
Tishya Chhabra <tatooine888@gmail.com>
2020-06-23 20:38:12 -0700

    created and finished posNeg

---
+++ warmup1/posNeg.java
@@ -0,0 +1,19 @@
+package warmup1;
+
+public class posNeg {
+    public boolean posNeg(int a, int b, boolean negative) {
+  
+        if(negative){
+          if(a < 0 && b < 0) return true;
+          else return false;
+          
+        } else {
+          if(a < 0 && b > 0) return true;
+          else if(b < 0 && a > 0) return true;
+          else return false;
+          
+        }
+        
+      }   
+    
+}
