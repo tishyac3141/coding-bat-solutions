b7e62c4a1aa4471a379f9cd494d5f9af6d0d39ce
Tishya Chhabra <tatooine888@gmail.com>
2020-06-23 23:18:37 -0700

    created and finished makeChocolate

---
+++ logic2/makeChocolate.java
@@ -0,0 +1,27 @@
+package logic2;
+
+public class makeChocolate {
+
+    public int makeChocolate(int small, int big, int goal) {
+        int weight = 0;
+        
+        for(int i = 0; i < big; i++){
+          weight += 5;
+        }
+        
+        while(weight > goal){
+          weight -= 5;
+        } 
+      
+        if(weight == goal) return 0;
+        
+        for(int i = 1; i <= small; i++){
+          weight++;
+          if(weight == goal) return i;
+        }
+        
+        return -1;
+        
+      }
+    
+}
