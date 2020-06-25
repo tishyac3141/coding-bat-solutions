6ea4aa0438b7183f36dab84bfebae411822f6c18
Tishya Chhabra <tatooine888@gmail.com>
2020-06-25 11:37:27 -0700

    created and finished isEverywhere

---
+++ array2/isEverywhere.java
@@ -0,0 +1,18 @@
+package array2;
+
+public class isEverywhere {
+    
+    public boolean isEverywhere(int[] nums, int val) {
+        boolean isEverywhere = true;
+        
+        for(int i = 0; i < nums.length - 1; i++){
+          if(!(nums[i] == val || nums[i + 1] == val)){
+            isEverywhere = false;
+          } 
+        }
+        
+        return isEverywhere;
+        
+      }
+
+}
