a2ab2a1a23ea646c55e0decb7ee12bad474d8626
Tishya Chhabra <tatooine888@gmail.com>
2020-06-25 12:08:12 -0700

    did a couple more string problems

---
+++ string1/comboString.java
@@ -0,0 +1,14 @@
+package string1;
+
+public class comboString {
+    
+    public String comboString(String a, String b) {
+        if(a.length() < b.length()){
+          return a + b + a;
+        } else {
+          return b + a + b;
+        }
+      }
+      
+    
+}
---
+++ string1/nonStart.java
@@ -0,0 +1,11 @@
+package string1;
+
+public class nonStart {
+
+    public String nonStart(String a, String b) {
+  
+        return a.substring(1) + b.substring(1);
+        
+      }
+    
+}
