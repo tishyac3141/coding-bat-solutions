1dc2781de8be7828e3aa1ef41397a081f51f8902
Tishya Chhabra <tatooine888@gmail.com>
2020-06-26 13:16:14 -0700

    added left2 and right2

---
+++ string1/left2.java
@@ -0,0 +1,11 @@
+package string1;
+
+public class left2{
+    
+    public String left2(String str) {
+  
+        if(str.length() <= 2) return str;
+        else return str.substring(2) + str.substring(0, 2);
+        
+      }
+}
