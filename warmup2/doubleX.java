fb794e8519c6f6d7eb695db7a3083b21c9746657
Tishya Chhabra <tatooine888@gmail.com>
2020-06-27 13:42:18 -0700

    created and finished doubleX

---
+++ warmup2/doubleX.java
@@ -0,0 +1,20 @@
+package warmup2;
+
+public class doubleX {
+
+    boolean doubleX(String str) {
+        boolean isFirst = true;
+        
+        for(int i = 0; i < str.length(); i++){
+          if(str.charAt(i) == 'x' && isFirst){
+            isFirst = false;
+            if(i < str.length() - 1 && str.charAt(i + 1) == 'x') return true;
+            
+          }
+        }
+        
+        return false;
+        
+      }
+    
+}
