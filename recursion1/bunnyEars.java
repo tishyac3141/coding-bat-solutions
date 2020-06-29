711ea539fd1882c14d50acd17571e9df3dbbda38
Tishya Chhabra <tatooine888@gmail.com>
2020-06-29 09:41:04 -0700

    created and finished a couple recursion problems

---
+++ recursion1/bunnyEars.java
@@ -0,0 +1,16 @@
+package recursion1;
+
+public class bunnyEars {
+
+    public int bunnyEars(int bunnies) {
+  
+        if(bunnies == 0){
+          return 0;
+        }
+        
+        return 2 + bunnyEars(bunnies - 1);
+        
+      }
+      
+    
+}
