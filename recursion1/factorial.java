56eb7b6df9b498fbf01c7978dcbd9dd3032f9e5f
Tishya Chhabra <tatooine888@gmail.com>
2020-06-28 21:41:23 -0700

    created and finished factorial.java

---
+++ recursion1/factorial.java
@@ -0,0 +1,14 @@
+package recursion1;
+
+public class factorial {
+
+    public int factorial(int n) {
+  
+        if(n == 1){
+          return 1;
+        }
+        else return n * factorial(n-1);
+        
+      }
+    
+}
