6339e764439e3f415547423d419e098b6cca7c06
Tishya Chhabra <tatooine888@gmail.com>
2020-06-29 09:51:07 -0700

    some more recursion problems

---
+++ recursion1/sumDigits.java
@@ -0,0 +1,13 @@
+package recursion1;
+
+public class sumDigits {
+
+    public int sumDigits(int n) {
+  
+        if(n == 0) return 0;
+        
+        return n % 10 + sumDigits(n / 10);
+        
+      }
+    
+}
---
+++ recursion1/triangle.java
@@ -0,0 +1,14 @@
+package recursion1;
+
+public class triangle {
+
+    public int triangle(int rows) {
+  
+        if(rows == 0) return 0;
+        else if(rows == 1) return 1;
+        
+        return rows + triangle(rows - 1);
+        
+      }
+    
+}
