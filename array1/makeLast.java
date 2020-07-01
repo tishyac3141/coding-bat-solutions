e6156bd72ffcfb4209353d445e928d63fc72f1e4
Tishya Chhabra <tatooine888@gmail.com>
2020-07-01 08:45:51 -0700

    did a couple more problems

---
+++ array1/makeLast.java
@@ -0,0 +1,15 @@
+package array1;
+
+public class makeLast {
+
+    public int[] makeLast(int[] nums) {
+  
+        int[] newArray = new int[nums.length * 2];
+        newArray[newArray.length - 1] = nums[nums.length - 1];
+        
+        return newArray;
+        
+      }
+      
+    
+}
