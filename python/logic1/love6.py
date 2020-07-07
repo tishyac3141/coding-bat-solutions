5154ef8abd382dd069794565a500ef20d7e39a81
Tishya Chhabra <tatooine888@gmail.com>
2020-07-07 13:22:18 -0700

    edited love6 (python)

--- python/logic1/love6.py
+++ python/logic1/love6.py
@@ -1,2 +1,9 @@
+#codingbat link: https://codingbat.com/prob/p100958
+
 def love6(a, b):
   return ((a==6 or b == 6) or (abs(a - b) == 6 or a + b == 6))
+
+  #alternatively:
+  if(a == 6 or b == 6): return True
+  elif(a + b == 6 or abs(a - b) == 6): return True
+  else: return False    
