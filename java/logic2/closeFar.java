ea636d2961708eee175100fd22eea774f12d5d1f
Tishya Chhabra <tatooine888@gmail.com>
2020-07-07 18:20:10 -0700

    fixed package declaration

--- java/logic2/closeFar.java
+++ java/logic2/closeFar.java
@@ -1,23 +1,21 @@
-package logic2;
+package java.logic2;
 
 /**
  * codingbat link: https://codingbat.com/prob/p138990
  */
 
-public class closeFar{
+public class closeFar {
 
-    public boolean closeFar(int a, int b, int c){
-  
-            if(Math.abs(a - b) <= 1){
-               return (Math.abs(a - c) >= 2 && Math.abs(b - c) >= 2);
-            }
-            else if(Math.abs(a - c) <= 1){
-              return (Math.abs(a - b) >= 2 && Math.abs(b - c) >= 2);
-            }
-            else {
-              return false;
-            }
-            
-    }    
-    
+  public boolean closeFar(int a, int b, int c) {
+
+    if (Math.abs(a - b) <= 1) {
+      return (Math.abs(a - c) >= 2 && Math.abs(b - c) >= 2);
+    } else if (Math.abs(a - c) <= 1) {
+      return (Math.abs(a - b) >= 2 && Math.abs(b - c) >= 2);
+    } else {
+      return false;
+    }
+
+  }
+
 }
--- java/logic2/makeBricks.java
+++ java/logic2/makeBricks.java
@@ -1,11 +1,11 @@
-package logic2;
+package java.logic2;
 
 //codingbat link: https://codingbat.com/prob/p183562
 //supplementary: https://codingbat.com/doc/practice/makebricks-introduction.html
 
-public class makeBricks{
+public class makeBricks {
 
-    public boolean makeBricks(int small, int big, int goal){
+    public boolean makeBricks(int small, int big, int goal) {
         return false;
     }
 }
