373c6906b56b7ea09cbc7d865646f38fd0bfd00f
Tishya Chhabra <tatooine888@gmail.com>
2020-07-08 21:39:33 -0700

    created and finished gHappy (java)

--- java_code/string3/gHappy.java
+++ java_code/string3/gHappy.java
@@ -2,4 +2,26 @@ package java_code.string3;
 
 public class gHappy {
 
+    public boolean gHappy(String str) {
+        boolean isHappy = true;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'g') {
+
+                if (i == 0) {
+                    if (str.length() == 1 || str.charAt(i + 1) != 'g')
+                        isHappy = false;
+                } else if (i == str.length() - 1) {
+                    if (str.charAt(i - 1) != 'g')
+                        isHappy = false;
+                } else if (str.charAt(i + 1) != 'g' && str.charAt(i - 1) != 'g') {
+                    isHappy = false;
+                }
+
+            }
+        }
+
+        return isHappy;
+
+    }
 }
