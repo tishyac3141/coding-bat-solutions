603215bbb8e3bf18db185f441944eb42955bb469
Tishya Chhabra <tatooine888@gmail.com>
2020-07-17 16:28:24 -0700

    did some more java logic1 problems

---
+++ java_code/logic1/alarmClock.java
@@ -0,0 +1,20 @@
+package java_code.logic1;
+
+public class alarmClock {
+
+    public String alarmClock(int day, boolean vacation) {
+        if (vacation) {
+            if (day != 0 && day != 6)
+                return "10:00";
+            else
+                return "off";
+        } else {
+            if (day != 0 && day != 6)
+                return "7:00";
+            else
+                return "10:00";
+        }
+
+    }
+
+}
