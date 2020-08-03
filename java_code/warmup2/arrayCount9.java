5602bde3e5ca4806b1ec5194697ff0ee9875ff77
Tishya Chhabra <tatooine888@gmail.com>
2020-08-02 21:37:51 -0700

    created and finished arrayCount9 (java)

---
+++ java_code/warmup2/arrayCount9.java
@@ -0,0 +1,17 @@
+package java_code.warmup2;
+
+public class arrayCount9 {
+
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 9)
+                count++;
+        }
+
+        return count;
+
+    }
+
+}
