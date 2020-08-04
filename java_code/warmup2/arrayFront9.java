51753ec8cfd5a449314531f0c4773149d6e8fc47
Tishya Chhabra <tatooine888@gmail.com>
2020-08-03 20:48:21 -0700

    created and finished arrayFront9 (java)

---
+++ java_code/warmup2/arrayFront9.java
@@ -0,0 +1,23 @@
+package java_code.warmup2;
+
+public class arrayFront9 {
+
+    public boolean arrayFront9(int[] nums) {
+
+        if (nums.length < 4) {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 9)
+                    return true;
+            }
+            return false;
+        } else {
+            for (int i = 0; i < 4; i++) {
+                if (nums[i] == 9)
+                    return true;
+            }
+            return false;
+        }
+
+    }
+
+}
