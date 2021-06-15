8f8c35bf24b0dcbc881871d457a21af8667f210c
Tishya Chhabra <tatooine888@gmail.com>
2021-06-15 10:46:28 -0700

    added comments

--- python/list2/count_evens.py
+++ python/list2/count_evens.py
@@ -1,14 +1,24 @@
 def count_evens(nums):
-    #normal way:
+    #"normal" way:
+
+    #set a counter variable
     numOfEvens = 0
+
+    #loop through the whole list
     for i in range(0, len(nums)):
+        #if, when we divide the number at that position by 2, the remainder is 0,
+        #then that means the number is even
         if(nums[i] % 2 == 0):
+            #increment the counter variable
             numOfEvens = numOfEvens + 1
 
     return numOfEvens
 
 def count_evens_2(nums):
     #using list comprehension:
+
+    #create a new list with only the elements that are even
     evens = [num for num in nums if num % 2 == 0]
+    #return the length of this new list
     return len(evens)
 
--- python/list2/has_22.py
+++ python/list2/has_22.py
@@ -1,6 +1,14 @@
 def has_22(nums):
+    #loop through the given list - BUT be sure that the bounds are 
+    #one less than the end of the array since we'll be checking
+    #both the element at the index we're at AND the element at 
+    #the index after. alternatively, you can start from index 1
+    #and check the element we're at the and the element before
     for i in range(0, len(nums) - 1):
+        #if the index we're at and the index after are both 2, then 
+        #we return True
         if(nums[i] == 2 and nums[i+1] == 2):
             return True
-    
+    #if we've exited the loop, that means consecutive 2's have not been found
+    #and we can just return false
     return False
